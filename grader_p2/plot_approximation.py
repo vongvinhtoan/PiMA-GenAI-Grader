b8fa28b04111e313d3c5cccd2dc9b594187edf5a
toanvv.ti.1922@gmail.com <toanvv.ti.1922@gmail.com>
2025-03-26 18:38:44 +0700

    update grader_p2

--- grader_p2/plot_approximation.py
+++ grader_p2/plot_approximation.py
@@ -6,17 +6,17 @@ from tqdm import tqdm
 
 def plot_approximation(
     student_id: Any,
-    approxer: Callable[[], float],
+    approx_func: Callable[[], float],
     tqdm_desc: str,
     var_name: str,
-    real_ans: Any,
+    true_value: Any,
     method_name: str
 ):
     random.seed(student_id)
 
     num_trials = 10000
     approximations = [
-        approxer()
+        approx_func()
         for _ in tqdm(range(num_trials), desc=tqdm_desc)
     ]
 
@@ -34,7 +34,7 @@ def plot_approximation(
     plt.figure(figsize=(10, 6))
     plt.hist(approximations, bins=30, color='blue', edgecolor='black', alpha=0.7)
     plt.axvline(x=sample_mean, color='green', linestyle='dashed', linewidth=2, label=f"Trung bình mẫu: {sample_mean:.5f}")
-    plt.axvline(x=real_ans, color='red', linestyle='dashed', linewidth=2, label=f"Giá trị thực của $\pi$: {real_ans}")
+    plt.axvline(x=true_value, color='red', linestyle='dashed', linewidth=2, label=f"Giá trị thực của $\pi$: {true_value}")
     plt.xlabel(f"Giá trị {var_name} xấp xỉ")
     plt.ylabel("Tần số")
     plt.title(f"Biểu đồ phần bố xấp xỉ {var_name} bằng {method_name}\nTrung bình mẫu: {sample_mean:.5f}, Phương sai mẫu: {sample_variance:.5f}")
