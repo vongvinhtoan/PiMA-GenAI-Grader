ee89841c70b148d663c013a6266c8b01f692e5f8
vongvinhtoan <toanvv.ti.1922@gmail.com>
2025-04-10 21:05:56 +0700

    Refactor grading logic in grade_p1 for clarity and efficiency; integrate tqdm for progress tracking

--- grader_homework_2/grade_p1.py
+++ grader_homework_2/grade_p1.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 from scipy.stats import norm
+from tqdm import tqdm
 
 def MonteCarlo(q_sampler, f, N):
     samples = [q_sampler() for _ in range(N)]
@@ -19,32 +20,27 @@ def grade(
             N = N
         )
 
-    print(f"Grading problem 1")
-
     num_trials = 10
     num_success = 0
     alpha = 0.05
     N = 50
 
     params = [
-        sorted((random.uniform(0, 100), random.uniform(0, 100)))
+        (random.uniform(0, 100), random.uniform(0, 100))
         for _ in range(num_trials)
     ]
 
-    for i in range(num_trials):
+    for i in tqdm(range(num_trials), desc="Grading problem 1", unit="trial"):
         l, t = params[i]
 
-        p = 2 * l / np.pi / t
+        if l <= t:
+            p = 2 * l / np.pi / t
+        else:
+            p = 2 / np.pi * np.arccos(t / l) + 2 / np.pi * l / t * (1 - np.sqrt(1-t**2 / l**2))
+
         p_hat = approx(l, t, N)
         var = p * (1 - p) / N
         z = (p_hat - p) / np.sqrt(var)
         p_value = 2 * (1 - norm.cdf(abs(z)))
 
-        print(f"Trial {i+1}: l = {l:.2f}, t = {t:.2f}, p = {p:.4f}, p_hat = {p_hat:.4f}, p_value = {p_value:.4f}")
-        if p_value > alpha:
-            num_success += 1
-            print(f"Trial {i+1} passed")
-        else:
-            print(f"Trial {i+1} failed")
     print(f"Number of trials passed: {num_success}/{num_trials}")
-    print(f"Grading problem 1 complete")
